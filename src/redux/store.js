25d241a88ff5b78e4f9d81d7a8bca4cb65fdf760
NatalieFalach <natkacandy@gmail.com>
2023-02-23 22:31:42 +0200

    middleware added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './slicers/filterSlice';
 import { contactsReducer } from './slicers/contactsSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
